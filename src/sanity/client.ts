f50bbb25d3f8f51f051c3184d05eb87da003df73
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2026-03-13 23:43:52 -0400

    add vercel dep

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -4,7 +4,7 @@ import imageUrlBuilder, { type SanityImageSource } from '@sanity/image-url'
 export const sanityClient = createClient({
   projectId: 'g02lhe3y',
   dataset: 'production',
-  useCdn: false, // set to `false` to bypass the edge cache and avoid CORS issues in development
+  useCdn: true, // Use CDN for better performance and to avoid CORS issues
   apiVersion: '2026-03-13', // use current date (YYYY-MM-DD) to target the latest API version
   // token: process.env.SANITY_AUTH_TOKEN // Only if you want to update content with the client
 })
